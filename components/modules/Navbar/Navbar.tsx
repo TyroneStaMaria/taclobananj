db66ace5c1a78cd92e59ec8fd5639fcc5f3df0f1
Tyrone Sta. Maria <tyrone.stamaria@orangeandbronze.com>
2021-07-30 18:45:32 +0800

    added responsive navbar

--- components/modules/Navbar/Navbar.tsx
+++ components/modules/Navbar/Navbar.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import style from "./Navbar.module.scss";
 import Link from "next/link";
 import Button from "../../elements/Button/Button";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <nav className={style.navBar}>
       <div className="container mx-auto flex items-center justify-between">
@@ -15,28 +21,53 @@ const Navbar = () => {
           height={70}
         />
         <div>
-          <ul className={style.navItems}>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About us</Link>
-            </li>
-            <li>
-              <Link href="#">Products</Link>
-            </li>
-            <li>
-              <Link href="#">Training Center</Link>
-            </li>
-            <li>
-              <Link href="#">Contact Us</Link>
-            </li>
-            <li>
-              <Button href="#" variant="outline">
-                Register / Log in
-              </Button>
-            </li>
-          </ul>
+          <button
+            onClick={toggleNav}
+            className=" inline-flex p-3  rounded lg:hidden text-red ml-auto hover:text-red-light outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${showNav ? "left-0" : "-left-full"} ${
+              style.navItemContainer
+            }`}
+          >
+            <ul className={`${style.navItems}`}>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About us</Link>
+              </li>
+              <li>
+                <Link href="#">Products</Link>
+              </li>
+              <li>
+                <Link href="#">Training Center</Link>
+              </li>
+              <li>
+                <Link href="#">Contact Us</Link>
+              </li>
+              <li>
+                <Button href="#" variant="outline">
+                  Register / Log in
+                </Button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>

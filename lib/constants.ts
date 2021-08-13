cdd682fb05558b6cf2804e1b29b6735dd94a2c84
Tyrone Sta. Maria <tyrone.stamaria@orangeandbronze.com>
2021-08-13 19:42:58 +0800

    converted to graphql

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,25 @@
-export const API_URL: string = "https://wp.taclobananjph.com/wp-json";
+export const API_URL: string = "https://wp.taclobananjph.com";
 
 export const CONTACT_FORM_API_URL: string =
-  API_URL + "/contact-form-7/v1/contact-forms/7/feedback";
+  API_URL + "/wp-json/contact-form-7/v1/contact-forms/7/feedback";
 
-export const BRANDS_API_URL: string = API_URL + "/wp/v2/brands?_embed";
+export const GRAPHQL_URI: string = API_URL + "/graphql/";
+
+export const BRANDS_QUERY: string = `query brandsQuery {
+  brands {
+    nodes {
+      brandId
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      title(format: RENDERED)
+    }
+  }
+}
+`;
+
+// export const BRANDS_API_URL: string = API_URL + "/wp/v2/brands?_embed";
 
 // export const Constants = [API_URL, CONTACT_FORM_API_URL, BRANDS_API_URL];

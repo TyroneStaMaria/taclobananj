4d212ac446fd47b8df5df6f0978b1fbaa6cbc386
Tyrone Sta. Maria <tyrone.stamaria@orangeandbronze.com>
2021-10-21 19:24:06 +0800

    seo changes

--- components/elements/Seo/Seo.tsx
+++ components/elements/Seo/Seo.tsx
@@ -8,16 +8,46 @@ const Seo = ({ data }) => {
       <Head>
         <title>{title} | Tacloban ANJ</title>
         <meta name="title" content={title} />
-        <meta name="og:title" property="og:title" content={title} />
-
         <meta name="description" content={description} />
+
+        <meta name="og:title" property="og:title" content={title} />
         <meta
           name="og:description"
           property="og:description"
           content={description}
         />
+        <meta
+          name="og:image"
+          property="og:image"
+          content="/images/link-preview.png"
+        />
+        <meta
+          name="og:url"
+          property="og:url"
+          content="https://www.taclobananjph.com"
+        />
 
-        <meta name="twitter:card" content={description} />
+        <meta
+          name="twitter:card"
+          property="twitter:card"
+          content={description}
+        />
+        <meta name="twitter:title" property="twitter:title" content={title} />
+        <meta
+          name="twitter:description"
+          property="twitter:description"
+          content={description}
+        />
+        <meta
+          name="twitter:url"
+          property="twitter:url"
+          content="https://www.taclobananjph.com"
+        />
+        <meta
+          name="twitter:image"
+          property="twitter:image"
+          content="/images/link-preview.png"
+        />
       </Head>
     </div>
   );
